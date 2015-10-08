7df8ae223c29215076047430a1aee461d147683c
Ishmael Smyrnow <ismyrnow@gmail.com>
2015-10-08 00:32:20 -0400

    Instructions for fulcrum example

--- fulcrum.js
+++ fulcrum.js
@@ -3,10 +3,12 @@ var eventProcessor = require('./event-processor');
 
 var app = express();
 
+// Handle GET requests to "/"
 app.get('/', function (req, res) {
   res.send('Hello :)');
 });
 
+// Handle POST requests to "/"
 app.post('/', function (req, res) {
   // Send the entire request body to the event processor.
   eventProcessor.process(req.body);
@@ -16,6 +18,7 @@ app.post('/', function (req, res) {
   res.status(202).end();
 });
 
+// Start the web server.
 app.listen(3000, function () {
   console.log('Server running on localhost:3000');
   console.log('Try posting some data to http://localhost:3000/')
